d2ebe7a1ce5f5f087ca062303d740373ef9db346
Aman9026 <amangupta884036@gmail.com>
2020-08-12 15:38:12 +0530

    mvp created

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+import 'package:splashscreen/splashscreen.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
   runApp(
-    MaterialApp(
-      home: MyAppLW(),
+    new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new MyApp(),
     ),
   );
+  FlutterStatusbarcolor.setStatusBarColor(Colors.blue.shade900);
 }
 
-class MyAppLW extends StatefulWidget {
+String cmd;
+web(i) async {
+  var url = "http://192.168.29.26/cgi-bin/web.py?x=$i";
+  var response = await http.get(url);
+  print(response.body);
+}
+
+class MyApp extends StatefulWidget {
   @override
-  _MyAppLWState createState() => _MyAppLWState();
+  _MyAppState createState() => new _MyAppState();
 }
 
-class _MyAppLWState extends State<MyAppLW> {
-  var cmd;
-  var webdata;
-
-  myweb(cmd) async {
-    var url = "http://192.168.0.162/cgi-bin/web.py?x=${cmd}";
-    var r = await http.get(url);
-
-    setState(() {
-      webdata = r.body;
-    });
-
-    // print(webdata);
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      title: new Text(
+        'Welcome to Docker Commands!',
+        style: new TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+            color: Colors.blue.shade900),
+      ),
+      seconds: 5,
+      navigateAfterSeconds: AfterSplash(),
+      image: new Image.asset('images/docker.gif'),
+      backgroundColor: Colors.blue.shade50,
+      styleTextUnderTheLoader: new TextStyle(),
+      photoSize: 160.0,
+      loaderColor: Colors.blue.shade900,
+    );
   }
+}
 
-/*
-  var db = ["vimal", "jack", "krish", "linux"];
-  var index = 0;
-  lwclick() {
-    setState(() {
-      index += 1;
-    });
-
-    print('clicked ...');
-    print("indexed changed");
-    print(index);
-  }
-*/
+class AfterSplash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('LW'),
+        elevation: 0,
+        backgroundColor: Colors.blue.shade900,
+        automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.8,
-          height: MediaQuery.of(context).size.height * 0.8,
-          color: Colors.grey,
-          child: Column(
-            children: <Widget>[
-              Text("Enter ur linux cmd :"),
-              TextField(
-                onChanged: (value) {
-                  cmd = value;
-                },
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              height: 600,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("images/best.png"),
+                ),
               ),
-              FlatButton(
-                  onPressed: () {
-                    myweb(cmd);
-                  },
-                  child: Text('click here')),
-              Text(webdata ?? "output is coming .."),
-            ],
-          ),
+            ),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: Text(
+                      "Docker at your service : ",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue.shade900,
+                      ),
+                    ),
+                  ),
+                  TextFormField(
+                    onChanged: (value) {
+                      cmd = value;
+                    },
+                    textAlign: TextAlign.left,
+                    autocorrect: false,
+                    autofocus: false,
+                    decoration: const InputDecoration(
+                      icon: Icon(Icons.comment),
+                      hintText: 'Enter Commands',
+                    ),
+                  ),
+                  FlatButton(
+                    color: Colors.blue.shade900,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    onPressed: () {
+                      web(cmd);
+                    },
+                    child: Text("RUN"),
+                    textColor: Colors.white,
+                  )
+                ])
+          ],
         ),
       ),
     );
